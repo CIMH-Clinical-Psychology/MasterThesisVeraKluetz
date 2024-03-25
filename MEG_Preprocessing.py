a9bb9f31fe167c49eb42aa791b9bcdb08b9851c9
skjerns <14980558+skjerns@users.noreply.github.com>
2024-03-25 15:18:35 +0100

    make suggestions

--- MEG_Preprocessing.py
+++ MEG_Preprocessing.py
@@ -3,7 +3,7 @@
 ### 22.02.2024
 ### vera.kluetz@zi-mannheim.de
 
-
+import os
 import mne
 import time
 from joblib import Memory
@@ -13,15 +13,18 @@ import autoreject
 import os
 import pandas as pd
 import numpy as np
+import settings
+
+# increase the niceness of the process on linux, i.e. give away  cpu resources
+# to other important processes if they need them (giving itself lower priority)
+os.nice(1)
 
 
 # -------------------- user specifics ----------------------------------------
 
 # set cache directory for faster execution with joblib Memory package
-cachedir = '/zi/flstorage/group_klips/data/data/VeraK/joblib_cache' #'/home/vera.kluetz/joblib_cache'
 
 # set file path, where the data can be found
-folderpath = (f"/zi/flstorage/group_klips/data/data/Emo-React-Prestudy/participant_data/")
 
 #set folderpath, where the resulting epochs should be stored
 epochs_folderpath = (f"/zi/flstorage/group_klips/data/data/VeraK/Prestudy_preprocessed_epochs/")
@@ -30,7 +33,7 @@ epochs_folderpath = (f"/zi/flstorage/group_klips/data/data/VeraK/Prestudy_prepro
 # -------------------- initial setup and function definition -----------------
 
 # Use the 'TkAgg' backend for interactive plotting
-plt.switch_backend('TkAgg')
+#plt.switch_backend('TkAgg')
 
 # measure code execution
 start_time = time.time()
@@ -41,39 +44,60 @@ warnings.filterwarnings("ignore",
                         category=RuntimeWarning, module="mne")
 
 # define cached functions for faster execution
-mem = Memory(cachedir)
+mem = Memory(settings.cachedir if settings.caching else None)  # only enable caching if wanted
 # cached_func = mne.cache(mne.io.read_raw)
 @mem.cache
 def read_raw_cached(fif_filepath):
-    raw = mne.io.read_raw_fif(fif_filepath, preload=True, verbose=False)
+    raw = mne.io.read_raw_fif(fif_filepath, preload=True, verbose='INFO')
     return raw
 
-@mem.cache
-def create_epochs_cached(raw, eve, event_id, tmin, tmax):
-    epochs = mne.Epochs(raw, events=eve, event_id=event_id, tmin=tmin, tmax=tmax, picks='meg')
-    return epochs
+
+def valid_filename(string):
+    """strips a string of all characters that might be problems for filenames
+    WARNING: only call this on filenames, not on filepaths containing '/' or '\\'
+    """
+    #  strip all special characters from the filename, else it causes problems
+    # all characters that are not alphanumeric will be replaced by _
+    allowed = ['_', '-', '.']
+    filtered = [x if (x.isalnum() or x in allowed) else '_'  for x in string]
+    return ''.join(filtered)
 
 @mem.cache
-def filter_cached(data_in, lower_bound, higher_bound):
-    data_out = data_in.filter(lower_bound, higher_bound)
-    return data_out
+def fit_apply_ica_cached(raw, ica_def):
+    """contains all ICA calculations,
+    expects a raw or epochs item
+    returns the same object but with EOG and ECG components removed"""
+    raw = raw.copy()  # work on a copy of the data, as some operations are in place
+    # manually save ICA results and hash them
+    assert isinstance(ica_def, dict), 'ica_def must be dictionary with arguments'
+    # define ICA within the function
+    ica = mne.preprocessing.ICA(**ica_def)
 
-@mem.cache
-def ica_fit_cached(ica_in, data):
-    ica_out = ica_in.fit(data, picks='meg')
-    return ica_out
+    # there seems to be a problem here! the filter length is too large for the
+    # epochs :-/ we might need to filter the raw object and then create epochs
+    raw_ica = raw.copy().filter(1, None, n_jobs=-1)  # should be quite fast
+    ica.fit(raw_ica, picks='meg')
+    idx_ecg, scores_ecg = ica.find_bads_ecg(raw, ch_name='BIO001')
+    idx_eog, scores_eog = ica.find_bads_eog(raw)
+    remove_components = idx_ecg + idx_eog
 
-@mem.cache
-def autoreject_fit_cached(ar_in, epochs):
-    ar_out = ar_in.fit(epochs)
-    #todo: here, I return the edited data and the resulting object, should I also do that for other cached functions?
-    #todo: Do I even correctly give out the edited epochs and not the original epochs?
-    return ar_out, epochs
+    # apply ICA to the raw object in place
+    ica.apply(raw, exclude=remove_components)
+    return raw, ica
 
-@mem.cache
-def ica_apply_cached(ica_in, epochs):
-    ica_out = ica_in.apply(epochs)
-    return ica_out
+def autoreject_fit_cached(epochs):
+    epochs_hash = hash(epochs)
+    autoreject_cache_file = valid_filename(f'{settings.cachedir}/{epochs_hash}.autoreject')
+
+    # if already computed, get the solution for this epochs object
+    if os.path.exists(autoreject_cache_file):
+        return autoreject.read_auto_reject(autoreject_cache_file)
+
+    # else compute it
+    ar = autoreject.AutoReject(n_jobs=-1, verbose=False)
+    ar.fit(epochs)
+    ar.save(autoreject_cache_file, overwrite=True)
+    return ar
 
 
 # -------------------- load data ---------------------------------------------
@@ -89,7 +113,7 @@ for participant in par_numbers:
     print(f'This is participant number {participant}')
     print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++')
 
-    fif_filepath = folderpath + f"ERP-{participant}/ERP{participant}_initial_tsss_mc.fif"
+    fif_filepath = settings.folderdir + f"ERP-{participant}/ERP{participant}_initial_tsss_mc.fif"
 
     # read raw data
     try:
@@ -98,7 +122,7 @@ for participant in par_numbers:
         print(
             f"Participant number {participant} does not exist or there has been problems with reading it's file. Proceeding with next participant.")
         continue
-    raw_copy = raw.copy()
+    # raw_copy = raw.copy()
 
     # check if we have more than 25 Minutes of Recordings included
     assert len(raw) > 60 * 25 * raw.info['sfreq']  # 50 sec * 25 min * sampling frequency
@@ -106,7 +130,6 @@ for participant in par_numbers:
     # Set EOG channel
     eogs = {'BIO002': 'eog', 'BIO003': 'eog'}
     raw.set_channel_types({**eogs})
-
 
     # -------------------- filtering -----------------------------------------
 
@@ -116,12 +139,11 @@ for participant in par_numbers:
     # raw.notch_filter(np.arange(50, 251, 50))
     # todo: is this clever?
     # raw.filter(0.1, 49)
-    raw = filter_cached(raw, 0.1, 49)
 
-
+    raw.filter(0.1, 49, n_jobs=-1)
     # -------------------- find events ---------------------------------------
 
-    eve = mne.find_events(raw, stim_channel='STI101', min_duration=3 / raw.info['sfreq'])
+    events = mne.find_events(raw, stim_channel='STI101', min_duration=3 / raw.info['sfreq'])
 
     # set event IDs
     event_id = {'trigger_preimage': 10,
@@ -133,41 +155,48 @@ for participant in par_numbers:
                 'trigger_flanker_start': 104}
 
     # only select events with a certain trigger
-    eve_id = {'trigger_gif_onset': 20}
-    events = eve[eve[:, 2] == 20]
+    event_id_selection = event_id['trigger_gif_onset']
+    # events = eve[eve[:, 2] == 20]
 
+    assert len(events)==1440, 'sanity check failed'
 
     # -------------------- create epochs -------------------------------------
 
     tmin, tmax = -0.5, 1
-    epochs = create_epochs_cached(raw, events, eve_id, tmin, tmax)
-    epochs.load_data()
+    # creating epochs is instantaneous, so does not need to be cached :)
+    # using `preload` prevents having to call "load_data" :)
+    # the events=events needs all event markers, and with the event_id you
+    # can subselecet which events to load
+    epochs = mne.Epochs(raw, events=events, event_id=event_id_selection,
+                        tmin=tmin, tmax=tmax, preload=True,
+                        baseline=None)  # don't subtract baseline
 
+    assert len(epochs)==144, f'sanity check failed, more or less than 144 epochs {len(epochs)=}'
     print(f"Bad channels: {epochs.info['bads']}")
 
     # Plot epochs
     #epochs.plot(show=False)
 
     # reject bad epochs automatically
+
     ar = autoreject.AutoReject(n_jobs=-1, verbose=False)
-    #todo: does the following substitution by cached function work properly?
-    #ar.fit(epochs)
-    ar, epochs = autoreject_fit_cached(ar, epochs)
+
+    ar = autoreject_fit_cached(epochs)
+
+    # call to ar.transform should be relatively fast, no caching needed
+    epochs = ar.transform(epochs)
 
     # create downsampled epochs
     # sampling_rate = 200
     # epochs_resampled = epochs.resample(sampling_rate, npad="auto")
 
-
     # -------------------- idependent component analysis ---------------------
-
-    epochs_ICA_for_fitting = filter_cached(epochs.copy(), 1, None)
-
     ica_method = 'fastica'
     n_components = 40  # todo: try with 50 maybe?
     random_state = 99
-    ica_def = mne.preprocessing.ICA(n_components=n_components, method=ica_method, random_state=random_state)
-    ica = ica_fit_cached(ica_def, epochs_ICA_for_fitting)
+    # store definition in dictionary
+    ica_def = dict(n_components=n_components, method=ica_method, random_state=random_state)
+    epochs, ica = fit_apply_ica_cached(epochs, ica_def)
 
     # check ICA solution
     explained_var_ratio = ica.get_explained_variance_ratio(epochs)  # todo: raw or raw_ICA_for_fitting?
@@ -177,33 +206,12 @@ for participant in par_numbers:
             f'{ratio}'
         )
 
-
-    # -------------------- reject EOG and ECG components ---------------------
-
-    # find bad ecg and eog channels
-    eog_indices, eog_scores = ica.find_bads_eog(raw)
-    ecg_indices, ecg_scores = ica.find_bads_ecg(raw, ch_name='BIO001')
-
-    # Apply ICA and exclude bad EOG components
-    #todo: I find the bad eog and ecg channels in the raw data and then remove those from the epochs, do the indices and still fit, then?
-    # todo: eog is two arrays in one array, and only one indice, so does it really exclude the correct component?
-    ica.exclude = eog_indices
-    ica.exclude.extend(eog_indices)
-    ica.exclude.extend(ecg_indices)
-    ica = ica_apply_cached(ica, epochs)
-
-
     #--------------------- save epochs ---------------------------------------
-    df = epochs.to_data_frame(index=["condition", "epoch", "time"])
-    df.sort_index(inplace=True)
-
-
-
-    filename_epoch = ("par" + str(participant) + "_" + str(next(iter(eve_id))) +"_"+ str(tmin) +"_"+ str(tmax) +"_"+ str(n_components))
+    filename_epoch = f'{participant=}_{event_id_selection=}_{tmin=}_{tmax=}-epo.fif'
+    filename_epoch = valid_filename(filename_epoch)
     epoch_file_path = os.path.join(epochs_folderpath, f"{filename_epoch}-epo.fif")
+    epochs.save(epoch_file_path, fmt='double', overwrite=True)
 
-    df.to_csv(os.path.join(epochs_folderpath, f"{filename_epoch}-epo.csv"))
-    #epochs.save(epoch_file_path, fmt='double', overwrite=True)
 
 
 end_time = time.time()
