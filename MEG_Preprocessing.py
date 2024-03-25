4f5ec88c96d0308b96bd7797fd931081af0e8221
skjerns <14980558+skjerns@users.noreply.github.com>
2024-03-25 16:34:34 +0100

    fix bugs

--- MEG_Preprocessing.py
+++ MEG_Preprocessing.py
@@ -72,11 +72,14 @@ def valid_filename(string):
     return filtered
 
 @mem.cache
-def fit_apply_ica_cached(mne_obj, ica_def):
+def fit_apply_ica_cached(mne_obj, mne_obj_meg, ica_def):
     """contains all ICA calculations,
+    mne_obj contains EOG and ECG data + MEG/EEG
+    mne_obj_meg contains only EOG and ECG data
     expects a raw or epochs item
     returns the same object but with EOG and ECG components removed"""
-    mne_obj = mne_obj.copy()  # work on a copy of the data, as some operations are in place
+    # work on a copy of the data, as some operations are in place
+    mne_obj_meg = mne_obj_meg.copy()
     # manually save ICA results and hash them
     assert isinstance(ica_def, dict), 'ica_def must be dictionary with arguments'
     # define ICA within the function
@@ -84,15 +87,16 @@ def fit_apply_ica_cached(mne_obj, ica_def):
 
     # there seems to be a problem here! the filter length is too large for the
     # epochs :-/ we might need to filter the raw object and then create epochs
-    raw_ica = mne_obj.copy().filter(1, None, n_jobs=-1)  # should be quite fast
+    raw_ica = mne_obj_meg.copy().filter(1, None, n_jobs=-1)  # should be quite fast
     ica.fit(raw_ica, picks='meg')
     idx_ecg, scores_ecg = ica.find_bads_ecg(mne_obj, ch_name='BIO001')
     idx_eog, scores_eog = ica.find_bads_eog(mne_obj)
     remove_components = idx_ecg + idx_eog
 
-    # apply ICA to the raw object in place
-    ica.apply(mne_obj, exclude=remove_components)
-    return mne_obj, ica
+    print('removing the following components: {remove_components}')
+    # apply ICA to the raw that only contains MEG data
+    ica.apply(mne_obj_meg, exclude=remove_components)
+    return mne_obj_meg, ica
 
 def autoreject_fit_cached(epochs):
     epochs_hash = hash(epochs)
@@ -103,9 +107,9 @@ def autoreject_fit_cached(epochs):
         return autoreject.read_auto_reject(autoreject_cache_file)
 
     # else compute it
-    ar = autoreject.AutoReject(n_jobs=-1, verbose=False, picks='mag',
+    ar = autoreject.AutoReject(n_jobs=-1, verbose=False, picks='meg',
                                random_state=99)
-    ar.fit(epochs.copy().pick('mag'))  # weirdly enough need to pick here again
+    ar.fit(epochs)  # weirdly enough need to pick here again
     ar.save(autoreject_cache_file, overwrite=True)
     return ar
 
@@ -127,6 +131,7 @@ for participant in par_numbers:
 
     # read raw data
     try:
+        print('###  loading and filtering data')
         # filter data already while loading :)
         raw = read_raw_filtered_cached(fif_filepath)
     except:
@@ -177,37 +182,43 @@ for participant in par_numbers:
     # using `preload` prevents having to call "load_data" :)
     # the events=events needs all event markers, and with the event_id you
     # can subselecet which events to load
-    epochs = mne.Epochs(raw, events=events, event_id=event_id_selection,
+    epochs_orig = mne.Epochs(raw, events=events, event_id=event_id_selection,
                         tmin=tmin, tmax=tmax, preload=True,
                         baseline=None)  # don't subtract baseline
 
-    assert len(epochs)==144, f'sanity check failed, more or less than 144 epochs {len(epochs)=}'
-    print(f"Bad channels: {epochs.info['bads']}")
+    epochs_meg = epochs_orig.copy().pick('meg') # create copy that only contains MEG data
+
+    assert len(epochs_meg)==144, f'sanity check failed, more or less than 144 epochs {len(epochs_meg)=}'
+    print(f"Bad channels: {epochs_meg.info['bads']}")
 
     # Plot epochs
     #epochs.plot(show=False)
 
     # reject bad epochs automatically
+    print('###  running autoreject')
 
-    ar = autoreject_fit_cached(epochs)
+    ar = autoreject_fit_cached(epochs_meg)
 
     # call to ar.transform should be relatively fast, no caching needed
-    epochs = ar.transform(epochs)
+    # there seems to be some bug that you can only enter channel data
+    # of MAG and GRAD, weird! I think this is a bug with autoreject.
+    epochs_meg = ar.transform(epochs_meg)
 
     # create downsampled epochs
     # sampling_rate = 200
     # epochs_resampled = epochs.resample(sampling_rate, npad="auto")
 
     # -------------------- idependent component analysis ---------------------
+    print('###  running ica')
     ica_method = 'fastica'
     n_components = 40  # todo: try with 50 maybe?
     random_state = 99
     # store definition in dictionary
     ica_def = dict(n_components=n_components, method=ica_method, random_state=random_state)
-    epochs, ica = fit_apply_ica_cached(epochs, ica_def)
+    epochs_meg, ica = fit_apply_ica_cached(epochs_orig, epochs_meg, ica_def)
 
     # check ICA solution
-    explained_var_ratio = ica.get_explained_variance_ratio(epochs)  # todo: raw or raw_ICA_for_fitting?
+    explained_var_ratio = ica.get_explained_variance_ratio(epochs_meg)  # todo: raw or raw_ICA_for_fitting?
     for channel_type, ratio in explained_var_ratio.items():
         print(
             f'Fraction of {channel_type} variance explained by all components: '
@@ -215,10 +226,11 @@ for participant in par_numbers:
         )
 
     #--------------------- save epochs ---------------------------------------
+    print('###  saving epochs')
     filename_epoch = f'{participant=}_{event_id_selection=}_{tmin=}_{tmax=}-epo.fif'
     filename_epoch = valid_filename(filename_epoch)
     epoch_file_path = os.path.join(epochs_folderpath, f"{filename_epoch}-epo.fif")
-    epochs.save(epoch_file_path, fmt='double', overwrite=True)
+    epochs_meg.save(epoch_file_path, fmt='double', overwrite=True)
 
 
 
