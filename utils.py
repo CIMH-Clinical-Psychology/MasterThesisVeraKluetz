4747fea1861bedc3fddaf7bcf0636671c7119818
Simon Kern <14980558+skjerns@users.noreply.github.com>
2024-03-26 19:53:55 +0100

    add loading function for participant data

---
+++ utils.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 17:35:53 2024
+
+This file contains various functions for helping with the workflow,
+e.g. loading of responses from participants
+
+@author: simon
+"""
+import os
+import settings
+import json
+import pandas as pd
+import pickle
+
+def load_responses(subj):
+    assert isinstance(subj, int)
+    subj_dir = settings.datadir + f'/ERP-{subj:02d}'
+
+    # load all files saved in participant's folder
+    files = os.listdir(subj_dir)
+
+    # select csv files
+    csv_files = [f for f in files if f.endswith('csv')]
+
+    # should be only one file, but maybe there are multiple, then
+    # the wrong ones should be moved to a separate folder manually
+    assert len(csv_files)>0, f'no csv found for subj ERP{subj:02d} at {subj_dir}'
+    assert len(csv_files)==1, f'more than one csv found for subj ERP{subj:02d} in {subj_dir}'
+
+    df = pd.read_csv(f'{subj_dir}/{csv_files[0]}')
+    assert (dflen:=len(df))==604, f'should be 604 but csv has only {dflen} rows'
+
+    # subselect trials without the learning trials in the beginning
+    df = df[~df.mp4_filename.isna()]
+    assert (df.Participant_ID==subj).all()
+
+    # drop all columns that are not relevant. Some might still be usefule and
+    # could be added later, for example the flanker task items
+    keep = ['mp4_filename', 'valence', 'emotion', 'gif_position',
+            'emo_arousal.rating', 'emo_valence.rating', 'emo_gifs_key.rt',
+            'feedback_valence_key.rt', 'feedback_arousal_key.rt']
+    df.drop(columns=[c for c in df.columns if not c in keep], inplace=True)
+
+    # remove redundant rows
+    df = df.iloc[::4].reset_index(drop=True)
+    df['button_pressed'] = ~df['emo_gifs_key.rt'].isna()
+    df.index.name = 'trial_nr'
+    assert (n_trials:=len(df))==144, f'more or less than 144 trials in file {n_trials=}'
+    return df
+
+csv_file = '/home/simon/Nextcloud/ZI/2023.05 EMO-REACT-prestudy/data/40_EMO_REACT_prestudy_2023-09-20_20h57.56.068.csv'
+
+if __name__=='__main__':
+    # by running this file as a main script you can run tests that check
+    # if all data can be loaded
+    for i in range(1,35):
+        if i in (25, 28, 31):  # these are missing
+            continue
+        load_responses(i)
