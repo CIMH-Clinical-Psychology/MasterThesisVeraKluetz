9477aaec75206782e3bed6d0e2de2859b76d5e7b
veraKl <74263871+veraKl@users.noreply.github.com>
2024-10-22 16:22:37 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ Created on Tue Mar 26 17:35:53 2024
 This file contains various functions for helping with the workflow,
 e.g. loading of responses from participants
 
-@author: simon kern
+@author: vera klütz and simon kern
 """
 import os
 import settings
